8b587d1826c4e19be9c7d6dc1bae3679f532b08e
Junkai336 <gjwnssla2@gmail.com>
2023-08-30 17:52:02 +0900

    0830

--- 52_js/part1/1000.practice_notest.js
+++ 52_js/part1/1000.practice_notest.js
@@ -1,2 +1,29 @@
+// 바꿀내용 드래그 + ctrl h = ctrl d d d d
+
+
 // 01_where
 
+/* 
+    한줄 주석: ctrl /
+    여러줄 주석: shift alt a
+
+    자바스크립트 코드 입력
+        - 대소문자 구분
+        - 문장 끝에 세미콜론(;)
+        - 문자열은 작은따옴표('') , 큰따옴표("")
+
+
+    외부 스크립트
+        - head나 body 태그 사이 기록 가능
+        - <script src="../js/sub4.js"></script>
+
+    내부 스크립트
+        - head 태그 사이 기록
+        <script>
+        document.write('javascript';)
+        </script>
+
+    인라인 스크립트
+        - HTML 속성 값 으로 표시
+        - <h1 onclick="document.write('Hello');">문자</h1>
+*/
