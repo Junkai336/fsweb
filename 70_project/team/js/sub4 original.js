eab5126b0e9628dd4660a9ed913f479717dcec3a
Junkai336 <gjwnssla2@gmail.com>
2023-09-07 17:49:07 +0900

    0907

--- 70_project/team/js/sub4 original.js
+++ 70_project/team/js/sub4 original.js
@@ -1,6 +1,3 @@
-
-
-
 // 
 // var lili = document.querySelectorAll(li);
 // console.log(lili);
@@ -78,32 +75,30 @@ channel_tab.addEventListener("click", ()=>{
     channel_tabtab.style.display = "flex";
 })
 
+// 모달 열기
+const mo1 = document.querySelector('.mo1');
+const mo2 = document.querySelector('.mo2');
+const mo3 = document.querySelector('.mo3');
+const mo4 = document.querySelector('.mo4');
+const mo5 = document.querySelector('.mo5');
+const mo6 = document.querySelector('.mo6');
+/* const mo7 = document.querySelector('.mo7');
+const mo8 = document.querySelector('.mo8');
+const mo9 = document.querySelector('.mo9');
+const mo10 = document.querySelector('.mo10');
+const mo11 = document.querySelector('.mo11');
+const mo12 = document.querySelector('.mo12'); */
+// const mo12 = document.getElementsByClassName('mo12');
 
-/* ******************************************************************************************************************************************************** */
-/* ******************************************************************************************************************************************************** */
-/* ******************************************************************************************************************************************************** */
 
-/* 
-    문제: 다른 탭에서 모달팝업창 안나옴
-    왜?: 변수지정이 하나밖에 안돼서 그런듯 (중복 클래스가 있긴 함 근데 명칭은 다른데?)
-    해결?: querySelectorAll , [0][1] 아이템리스트, consolelog로 순번 지정해서 ㄱㄱ
-    흠: 함수 하나 만들어서 코드길이 줄이는 것도 생각. 안되면 강사님 찬스 ㄱㄱ
-*/
 
-// 모달팝업창 (여기 수정)
-
-// 모달팝업창을 여는 게시글 선언
-const check = document.querySelectorAll('.momo');
-// 모달팝업창 여는 게시글 순번 확인
-console.log(check);
-
-// 모달팝업 박스와 창 선언
-const modal_container = document.querySelector('.modal_container');
-const modal_content = document.querySelectorAll('.momomo');
-// 모답팝업"창" 순번 확인
-console.log(modal_content);
-
-// 모달팝업창 열기
+const modal0 = document.querySelector('.modal_container');
+const modal1 = document.querySelector('.modal1');
+const modal2 = document.querySelector('.modal2');
+const modal3 = document.querySelector('.modal3');
+const modal4 = document.querySelector('.modal4');
+const modal5 = document.querySelector('.modal5');
+const modal6 = document.querySelector('.modal6');
 
 mo1.addEventListener("click", ()=>{
     modal0.style.display = "block";
@@ -165,9 +160,68 @@ mo6.addEventListener("click", ()=>{
     modal6.style.display = "block";
 })
 
+/* mo7.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "block";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
+})
 
-// 모달팝업창 닫기
+mo8.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "none";
+    modal2.style.display = "block";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
+})
 
+mo9.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "block";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
+})
+
+mo10.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "block";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
+})
+
+mo11.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "block";
+    modal6.style.display = "none";
+})
+
+mo12.addEventListener("click", ()=>{
+    modal0.style.display = "block";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "block";
+}) */
+
+
+// 모달 닫기
 const modal_close = document.querySelector('#modal_close_a');
 const modal_close2 = document.querySelector('#modal_close_b');
 const modal_close3 = document.querySelector('#modal_close_c');
