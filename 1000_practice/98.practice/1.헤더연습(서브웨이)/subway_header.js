f1922d89251438f4ddcf74497ace7b0e6cc17314
Junkai336 <gjwnssla2@gmail.com>
2023-09-11 17:52:30 +0900

    0911

--- 1000_practice/98.practice/1.헤더연습(서브웨이)/subway_header.js
+++ 1000_practice/98.practice/1.헤더연습(서브웨이)/subway_header.js
@@ -1,19 +1,35 @@
 const depth1 = document.querySelectorAll('li')
 
-
-const hamburger = document.querySelector('.hamburger')
+const div = document.querySelectorAll('div')
 const hamburgerSub = document.querySelector('.hamburger_sub')
-// const hamburgerClose = document.querySelector('.hamburger_close')
-const hamburgerClose = document.querySelector('.logo')
+const hamburger = document.querySelector('.hamburger')
 
 console.log(depth1);
+console.log(div);
+div.length;
 
-hamburger.addEventListener("click", () => {
+let i = 1;
+
+div[3].onclick = function() {
+    if (i===1) {
+        hamleft();
+        i++;
+    } else if (i===2) {
+        hamright();
+        i--;
+    }
+}
+
+console.log(i);
+
+function hamleft() {
     hamburgerSub.style.left = "-135px";
+    hamburger.style.color = '#fff';
     hamburgerSub.style.transition = ".5s";
-})
+}
 
-hamburgerClose.addEventListener("click", () => {
+function hamright() {
     hamburgerSub.style.left = "135px";
+    hamburger.style.color = '#000';
     hamburgerSub.style.transition = ".5s";
-})
+} 
