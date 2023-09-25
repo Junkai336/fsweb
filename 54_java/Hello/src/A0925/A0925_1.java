8f388d76ae2218727da490a7f886cded6c91640b
Junkai336 <gjwnssla2@gmail.com>
2023-09-25 17:48:25 +0900

    0925

---
+++ 54_java/Hello/src/A0925/A0925_1.java
@@ -0,0 +1,31 @@
+package A0925;
+
+public class A0925_1 {
+    public static void main(String[] args) {
+       int var1 = 10;
+       System.out.println(var1);
+       
+       int var2 = 20;
+       System.out.println(var2);
+       
+       int var3 = 0x10; //hexa 16진수 - 16
+       System.out.println(var3);
+
+       int var4 = (int)20.7;
+       System.out.println(var4);
+
+       Double var44 = 20.7;
+       System.out.println(var44);
+       
+    }
+}
+
+
+
+/* 
+    package : 폴더랑 비슷
+    클래스 안에는 하나의 메인 클래스 필요
+    int = 정수형 변수만 받는다.
+    Double? 
+
+ */
---
+++ 54_java/Hello/src/A0925/A0925_3.java
@@ -0,0 +1,18 @@
+package A0925;
+
+public class A0925_3 {
+    public static void main(String[] args) {
+        int value;
+        value = 10;
+        System.out.println(value);
+
+        int hour = 3;
+        int minute = 5;
+        System.out.println(hour + "시간" + minute + "분");
+
+        int totalMinute = (hour * 60) + minute;
+        System.out.println("총" + totalMinute + "분");
+    }
+}
+
+// 노란 밑줄: 아직 활용한 적이 없다.
