9ef58028d921f6d7a3e5cfc310267d6775a3a028
Junkai336 <gjwnssla2@gmail.com>
2023-10-19 17:49:26 +0900

    1019

---
+++ 54_java/Hello/src/A1019/a677/Coin.java
@@ -0,0 +1,14 @@
+package A1019.a677;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
