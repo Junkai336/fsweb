0a436e39af724fee629380b386bbf88e08ecf030
Junkai336 <gjwnssla2@gmail.com>
2023-11-10 08:21:04 +0900

    1109

--- 1000_practice/4.Java/Hello/hello/src/Chapter1/ch15.java
+++ 1000_practice/4.Java/Hello/hello/src/Chapter1/ch15.java
@@ -3,3 +3,65 @@ package Chapter1;
 public class ch15 {
     
 }
+
+/* 
+ * 컬렉션 프레임워크
+ *  - List : ArrayList, Vector, LinkedList
+ *      - 순서 유지, 중복 저장
+ *  - Set : HashSet, TreeSet
+ *      - 순서 유지X, 중복 저장X
+ *  - Map : HashMap, Hashtable, TreeMap, Properties
+ *      - 키와 값으로 구성된 엔트리 저장
+ *      - 키는 중복 저장X
+ * 
+ * List 컬렉션
+ *  - 객체를 저장하면 인덱스 부여
+ *  - 인덱스로 객체 검색, 삭제
+ *      공통사용 메소드
+ *          - add() : 객체를 맨 끝에 추가
+ *          - add(index, element) : 인덱스에 객체를 추가
+ *          - set(index, element) : 인덱스의 객체를 새로운 객체로 바꿈
+ *          - contains() : 객체가 저장되어 있는지 여부
+ *          - get() : 인덱스에 저장된 객체 리턴
+ *          - isEmpty() : 컬렉션이 비어있는지 조사
+ *          - size() : 전체 객체 수 리턴
+ *          - clear() : 모든 객체 삭제
+ *          - remove(index) : 인덱스에 저장된 객체 삭제
+ *          - remove(element) : 객체 삭제
+ * 
+ * ArrayList
+ *  - 객체를 추가하면 내부 배열에 객체 저장
+ *  - 일반 배열과 다르게 제한 없이 객체 추가 가능
+ *  - List<E> list = new ArrayList<E>();
+ *      - <E>에 지정된 타입의 객체만 저장
+ *  - List<E> list = new ArrayList<>();
+ *      - <E>에 지정된 타입의 객체만 저장
+ *  - List<> list = new ArrayList<>();
+ *      - 모든 타입의 객체 저장
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+
+
+
+
+
+
+
+
+
+
+
+
+ */
