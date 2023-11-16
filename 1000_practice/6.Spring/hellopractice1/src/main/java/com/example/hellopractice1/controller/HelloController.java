018102aacf70aab97e71116c667bcaba65106090
Junkai336 <gjwnssla2@gmail.com>
2023-11-16 17:48:59 +0900

    1116

--- 1000_practice/6.Spring/hellopractice1/src/main/java/com/example/hellopractice1/controller/HelloController.java
+++ 1000_practice/6.Spring/hellopractice1/src/main/java/com/example/hellopractice1/controller/HelloController.java
@@ -1,4 +1,17 @@
 package com.example.hellopractice1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping(value = "/")
+    public String toHello(Model model) {
+        model.addAttribute("gogo", "model.addAttribute");
+        return "/hello";
+    }
+
 }
---
+++ 55_spring/1116/shop/src/main/java/com/shop/entity/ItemSellStatus.java
@@ -0,0 +1,7 @@
+package com.shop.entity;
+
+public enum ItemSellStatus {
+
+    SELL, SOLD_OUT
+    // 열거 타입은 지정할 수 있다. 객체.열거목록 으로.
+}
